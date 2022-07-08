1eb4f7259bb7f82ddb90c0107eb9a28774e82eed
jalorenzol <lorenzollanesj@gmail.com>
2022-07-07 23:16:11 -0400

    Interacción y datos de los Selects y la gráfica actualizados

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Select from 'react-select'
 import "./App.css";
 import BarChart from "./components/BarChart";
-import { UserData } from "./Data";
+import { SalesData } from "./Data";
+import { SelectOptions } from "./Data";
+import { SelectTecnoOptions } from "./Data";
+import { SelectMiscelOptions } from "./Data";
+import { SelectFoodOptions } from "./Data";
+import { SelectHomeOptions } from "./Data";
+import { SelectLaptopsOptions } from "./Data";
+import { SelectDesktopOptions } from "./Data";
+import { SelectWomenswearOptions } from "./Data";
+import { SelectClothingOptions } from "./Data";
+import { SelectShoesOptions } from "./Data";
+import { SelectDessertsOptions } from "./Data";
+import { SelectMeatsOptions } from "./Data";
+import { SelectVegetablesOptions } from "./Data";
+import * as ReactDOM from 'react-dom';
 
-const options = [
-  { value: 'chocolate', label: 'Chocolate' },
-  { value: 'strawberry', label: 'Strawberry' },
-  { value: 'vanilla', label: 'Vanilla' }
-]
+// const options = [
+//   { value: 'chocolate', label: 'Chocolate' },
+//   { value: 'strawberry', label: 'Strawberry' },
+//   { value: 'vanilla', label: 'Vanilla' }
+// ]
+// const options2 = [
+//   { value: "rico", label: 'Rico' }
+// ]
 
+// const options = SalesData.
 
 function App() {
-  const [userData, setUserData] = useState({
-    labels: UserData.map((data) => data.year),
+  const [userData] = useState({
+    labels: SalesData.map((data) => data.month),
     datasets: [
       {
-        label: "Users Gained",
-        data: UserData.map((data) => data.userGain),
+        label: "Product sales",
+        data: SalesData.map((data) => data.sales),
         backgroundColor: [
           "rgba(75,192,192,1)",
           "#ecf0f1",
@@ -33,20 +51,118 @@ function App() {
   });
 
 
+
+  const optionSelect = (e) => {
+    if (e.value === SelectOptions[0].value) {
+      ReactDOM.render(<Select
+        id="2"
+        options={SelectTecnoOptions} 
+        onChange={typesSelect}/>,
+        document.getElementById("2"));
+    }
+    else if (e.value === SelectOptions[1].value) {
+      ReactDOM.render(<Select
+        id="2"
+        options={SelectMiscelOptions}
+        onChange={typesSelect} />,
+        document.getElementById("2"));
+    }
+    else if (e.value === SelectOptions[2].value) {
+      ReactDOM.render(<Select
+        id="2"
+        options={SelectFoodOptions}
+        onChange={typesSelect} />,
+        document.getElementById("2"));
+    }
+    console.log(e);;
+  };
+
+  const typesSelect = (e) => {
+    if (e.value === SelectTecnoOptions[0].value) {
+      ReactDOM.render(<Select
+        id="3"
+        options={SelectHomeOptions} />,
+        document.getElementById("3"));
+    }
+    else if (e.value === SelectTecnoOptions[1].value) {
+      ReactDOM.render(<Select
+        id="3"
+        options={SelectLaptopsOptions} />,
+        document.getElementById("3"));
+    }
+    else if (e.value === SelectTecnoOptions[2].value) {
+      ReactDOM.render(<Select
+        id="3"
+        options={SelectDesktopOptions} />,
+        document.getElementById("3"));
+    }
+    else if (e.value === SelectMiscelOptions[0].value) {
+      ReactDOM.render(<Select
+        id="3"
+        options={SelectWomenswearOptions} />,
+        document.getElementById("3"));
+    }
+    else if (e.value === SelectMiscelOptions[1].value) {
+      ReactDOM.render(<Select
+        id="3"
+        options={SelectClothingOptions} />,
+        document.getElementById("3"));
+    }
+    else if (e.value === SelectMiscelOptions[2].value) {
+      ReactDOM.render(<Select
+        id="3"
+        options={SelectShoesOptions} />,
+        document.getElementById("3"));
+    }
+    else if (e.value === SelectFoodOptions[0].value) {
+      ReactDOM.render(<Select
+        id="3"
+        options={SelectDessertsOptions} />,
+        document.getElementById("3"));
+    }
+    else if (e.value === SelectFoodOptions[1].value) {
+      ReactDOM.render(<Select
+        id="3"
+        options={SelectMeatsOptions} />,
+        document.getElementById("3"));
+    }
+    else if (e.value === SelectFoodOptions[2].value) {
+      ReactDOM.render(<Select
+        id="3"
+        options={SelectVegetablesOptions} />,
+        document.getElementById("3"));
+    }
+    console.log(e);;
+  };
+
+
   return (
     <div className="App">
 
       <div style={{ width: 700 }}>
-        <div style={{width: "100%",display: "flex", flexWrap: "wrap",
-  flexDirection: "row", justifyContent: "space-between"}}>
+        <div style={{
+          width: "100%", display: "flex", flexWrap: "wrap",
+          flexDirection: "row", justifyContent: "space-between"
+        }}>
           <div>
-            <Select options={options} />
+            <Select
+              id="1"
+              placeholder="Categories"
+              options={SelectOptions}
+              onChange={optionSelect}
+            />
           </div>
           <div >
-            <Select options={options} />
+            <Select
+              id="2"
+              placeholder="Tipos"
+               />
           </div>
           <div >
-            <Select options={options} />
+            <Select
+              id="3"
+              placeholder="Marcas"
+               />
           </div>
         </div>
         <BarChart chartData={userData} />
--- src/Data.js
+++ src/Data.js
@@ -1,32 +1,221 @@
-export const UserData = [
+export const SalesData = [
   {
     id: 1,
-    year: 2016,
-    userGain: 80000,
-    userLost: 823,
+    month: "January",
+    sales: 150,
   },
   {
     id: 2,
-    year: 2017,
-    userGain: 45677,
-    userLost: 345,
+    month: "February",
+    sales: 80,
   },
   {
     id: 3,
-    year: 2018,
-    userGain: 78888,
-    userLost: 555,
+    month: "March",
+    sales: 120,
   },
   {
     id: 4,
-    year: 2019,
-    userGain: 90000,
-    userLost: 4555,
+    month: "April",
+    sales: 50,
   },
   {
     id: 5,
-    year: 2020,
-    userGain: 4300,
-    userLost: 234,
+    month: "May",
+    sales: 70,
   },
 ];
+
+export const SelectOptions = [
+  {
+    value: 'tecno',
+    label: 'Technology'
+  },
+  {
+    value: 'miscel',
+    label: 'Miscellany'
+  },
+  {
+    value: 'food',
+    label: 'Food'
+  }  
+]
+
+export const SelectTecnoOptions = [
+  {
+    value: 'home',
+    label: 'Home Appliances'
+  },
+  {
+    value: 'laptops',
+    label: 'Laptops'
+  },
+  {
+    value: 'desktop',
+    label: 'Desktop Computers'
+  }  
+]
+
+export const SelectMiscelOptions = [
+  {
+    value: 'womenswear',
+    label: 'Womenswear'
+  },
+  {
+    value: 'clothing',
+    label: 'Mens Clothing'
+  },
+  {
+    value: 'shoes',
+    label: 'Shoes'
+  }  
+]
+
+export const SelectFoodOptions = [
+  {
+    value: 'desserts',
+    label: 'Desserts'
+  },
+  {
+    value: 'meats',
+    label: 'Meats'
+  },
+  {
+    value: 'vegetables',
+    label: 'Vegetables'
+  }  
+]
+export const SelectHomeOptions = [
+  {
+    value: 'daytron',
+    label: 'Daytron'
+  },
+  {
+    value: 'creative',
+    label: 'Creative'
+  },
+  {
+    value: 'samsung',
+    label: 'Samsung'
+  }  
+]
+
+export const SelectLaptopsOptions = [
+  {
+    value: 'msi',
+    label: 'MSI'
+  },
+  {
+    value: 'mac',
+    label: 'MAC'
+  },
+  {
+    value: 'dell',
+    label: 'DELL'
+  }  
+]
+
+export const SelectDesktopOptions = [
+  {
+    value: 'msi',
+    label: 'MSI'
+  },
+  {
+    value: 'mac',
+    label: 'MAC'
+  },
+  {
+    value: 'dell',
+    label: 'DELL'
+  }  
+]
+
+export const SelectWomenswearOptions = [
+  {
+    value: 'mango',
+    label: 'Mango'
+  },
+  {
+    value: 'cortefiel',
+    label: 'Cortefiel'
+  },
+  {
+    value: 'zara',
+    label: 'Zara'
+  }  
+]
+
+export const SelectClothingOptions = [
+  {
+    value: 'gucci',
+    label: 'Gucci'
+  },
+  {
+    value: 'prada',
+    label: 'Prada'
+  },
+  {
+    value: 'fendi',
+    label: 'Fendi'
+  }  
+]
+
+export const SelectShoesOptions = [
+  {
+    value: 'gucci',
+    label: 'Gucci'
+  },
+  {
+    value: 'nike',
+    label: 'Nike'
+  },
+  {
+    value: 'adidas',
+    label: 'Adidas'
+  }  
+]
+
+export const SelectDessertsOptions = [
+  {
+    value: 'ricolino',
+    label: 'Ricolino'
+  },
+  {
+    value: 'halls',
+    label: 'Halls'
+  },
+  {
+    value: 'lucas',
+    label: 'Lucas'
+  }  
+]
+
+export const SelectMeatsOptions = [
+  {
+    value: 'sahi',
+    label: 'SAHI'
+  },
+  {
+    value: 'angus',
+    label: 'ANGUS'
+  },
+  {
+    value: 'jack',
+    label: 'JACK'
+  }  
+]
+
+export const SelectVegetablesOptions = [
+  {
+    value: 'augason',
+    label: 'Augason'
+  },
+  {
+    value: 'honey',
+    label: 'Honeyville'
+  },
+  {
+    value: 'jack',
+    label: 'JACK'
+  }  
+]
